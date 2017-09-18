1421b41779871561ddb73bf51b42ee644dfb5010
lokiloko <ianw.tkj2@gmail.com>
2017-09-18 22:30:06 +0700

    remake numberToWords code

--- number_in_words.js
+++ number_in_words.js
@@ -1,237 +1,58 @@
 function numberToWords(number) {
   // Your code here
-  if(!number > 0){
-    return "";
+  var daftarAngka = ['','satu','dua','tiga','empat','lima','enam','tujuh', 'delapan','sembilan','sepuluh','sebelas'];
+  if(number<=11){
+    return daftarAngka[number];
   }
-  var daftarKata = ['puluh','ratus','ribu','juta','milliar','triliun', 'belas'];
-  var daftarAngka = ['se', 'dua','tiga','empat','lima','enam','tujuh','delapan','sembilan']
-  var hasil = "";
-  var strNumber = number.toString().split('');
-  //triliun
-  if(strNumber.length > 12){
-    switch(strNumber.length - 12){
-      case 1:
-        daftarAngka[0] = 'satu';
-        hasil += daftarAngka[parseInt(strNumber[0]) - 1];
-        strNumber.shift();
-        break;
-      case 2:
-        if(strNumber[0] === '1'){
-          hasil += daftarAngka[parseInt(strNumber[1] - 1)];
-          if(strNumber[0] !== '1')
-            hasil+= " ";
-          hasil += daftarKata[6] + " ";
-          strNumber.shift();
-          strNumber.shift();
-        }
-        else{
-          hasil += daftarAngka[parseInt(strNumber[0]) - 1];
-          if(strNumber[0] !== '1')
-            hasil+= " ";
-          hasil += daftarKata[0] + " ";
-          strNumber.shift();
-        }
-        break;
-      case 3:
-        hasil += daftarAngka[parseInt(strNumber[0]) - 1];
-        if(strNumber[0] !== '1')
-          hasil+= " ";
-        hasil += daftarKata[1] + " ";
-        strNumber.shift();
-        break;
-    }
-    var newNumber = parseInt(strNumber.join(''));
-    if(strNumber.length > 12)
-      return hasil + numberToWords(newNumber);
-    else{
-      return hasil + " " + daftarKata[5] +" " + numberToWords(newNumber);
-    }
+  if(number<=19){
+    return daftarAngka[number] + " belas " + numberToWords(number-10);
   }
-  //miliar
-  if(strNumber.length > 9){
-    switch(strNumber.length - 9){
-      case 1:
-        daftarAngka[0] = 'satu';
-        hasil += daftarAngka[parseInt(strNumber[0]) - 1];
-        strNumber.shift();
-        break;
-      case 2:
-        if(strNumber[0] === '1'){
-          hasil += daftarAngka[parseInt(strNumber[1] - 1)];
-          if(strNumber[0] !== '1')
-            hasil+= " ";
-          hasil += daftarKata[6] + " ";
-          strNumber.shift();
-          strNumber.shift();
-        }
-        else{
-          hasil += daftarAngka[parseInt(strNumber[0]) - 1];
-          if(strNumber[0] !== '1')
-            hasil+= " ";
-          hasil += daftarKata[0] + " ";
-          strNumber.shift();
-        }
-        break;
-      case 3:
-        hasil += daftarAngka[parseInt(strNumber[0]) - 1];
-        if(strNumber[0] !== '1')
-          hasil+= " ";
-        hasil += daftarKata[1] + " ";
-        strNumber.shift();
-        break;
-    }
-    var newNumber = parseInt(strNumber.join(''));
-    if(strNumber.length > 9)
-      return hasil + numberToWords(newNumber);
-    else{
-      return hasil + " " + daftarKata[4] +" " + numberToWords(newNumber);
-    }
+  if(number<=99){
+    return daftarAngka[Math.floor(number/10)] + " puluh " + numberToWords(number%10);
   }
-  //juta
-  if(strNumber.length > 6){
-    if(strNumber.length === 4 && strNumber[1] === '0' && strNumber[2] === '0'){
-      if(strNumber[0] !== '1'){
-        hasil += daftarAngka[parseInt(strNumber[0]) - 1] + " ";
-      }
-      else{
-        hasil += daftarAngka[parseInt(strNumber[0]) - 1];
-      }
-      strNumber.shift();
-      strNumber.shift();
-      strNumber.shift();
-      return hasil + "ribu " + numberToWords(strNumber);
-    }
-    daftarAngka[0] = 'satu';
-    switch(strNumber.length - 6){
-      case 1:
-        daftarAngka[0] = 'satu';
-        hasil += daftarAngka[parseInt(strNumber[0]) - 1];
-        strNumber.shift();
-        break;
-      case 2:
-        if(strNumber[0] === '1'){
-          hasil += daftarAngka[parseInt(strNumber[1] - 1)];
-          if(strNumber[0] !== '1')
-            hasil+= " ";
-          hasil += daftarKata[6] + " ";
-          strNumber.shift();
-          strNumber.shift();
-        }
-        else{
-          hasil += daftarAngka[parseInt(strNumber[0]) - 1];
-          if(strNumber[0] !== '1')
-            hasil+= " ";
-          hasil += daftarKata[0] + " ";
-          strNumber.shift();
-        }
-        break;
-      case 3:
-        hasil += daftarAngka[parseInt(strNumber[0]) - 1];
-        if(strNumber[0] !== '1')
-          hasil+= " ";
-        hasil += daftarKata[1] + " ";
-        strNumber.shift();
-        break;
-    }
-    var newNumber = parseInt(strNumber.join(''));
-    if(strNumber.length > 6)
-      return hasil + numberToWords(newNumber);
-    else{
-      return hasil + " " + daftarKata[3] + " " + numberToWords(newNumber);
-    }
+  if(number<=199){
+    return "seratus " + numberToWords(number-100);
   }
-
-  //ribu
-  if(strNumber.length > 3){
-    if(strNumber.length === 4 && strNumber[0] === '1'){
-      daftarAngka[0] = 'se';
-      hasil += daftarAngka[parseInt(strNumber[0]) - 1];
-      strNumber.shift();
-    }
-    switch(strNumber.length - 3){
-      case 1:
-        daftarAngka[0] = 'satu';
-        hasil += daftarAngka[parseInt(strNumber[0]) - 1];
-        strNumber.shift();
-        break;
-      case 2:
-        if(strNumber[0] === '1'){
-          hasil += daftarAngka[parseInt(strNumber[1] - 1)];
-          if(strNumber[0] !== '1')
-            hasil+= " ";
-          hasil += daftarKata[6] + " ";
-          strNumber.shift();
-          strNumber.shift();
-        }
-        else{
-          hasil += daftarAngka[parseInt(strNumber[0]) - 1];
-          if(strNumber[0] !== '1')
-            hasil+= " ";
-          hasil += daftarKata[0] + " ";
-          strNumber.shift();
-        }
-        break;
-      case 3:
-        hasil += daftarAngka[parseInt(strNumber[0]) - 1];
-        if(strNumber[0] !== '1')
-          hasil+= " ";
-        hasil += daftarKata[1] + " ";
-        strNumber.shift();
-        break;
-    }
-    var newNumber = parseInt(strNumber.join(''));
-    if(strNumber.length > 3)
-      return hasil + numberToWords(newNumber);
-    else{
-      if(hasil[hasil.length-1] === 'e'){
-        return hasil + daftarKata[2] + " " + numberToWords(newNumber);
-      }
-        return hasil + " " + daftarKata[2] + " " + numberToWords(newNumber);
-    }
+  if(number<=999){
+    return daftarAngka[Math.floor(number/100)] + " ratus " + numberToWords(number % 100);
   }
-
-  //ratus
-  if(strNumber.join('') === '1'){
-    hasil += 'satu'
-    strNumber.shift();
+  if(number<=1999){
+    return "seribu " + numberToWords(number-1000);
   }
-  switch(strNumber.length){
-    case 1:
-      hasil += daftarAngka[parseInt(strNumber[0]) - 1];
-      strNumber.shift();
-      break;
-    case 2:
-      if(strNumber[0] === '1'){
-        hasil += daftarAngka[parseInt(strNumber[1] - 1)];
-        if(strNumber[0] !== '1')
-          hasil+= " ";
-        hasil += daftarKata[6] + " ";
-        strNumber.shift();
-        strNumber.shift();
-      }
-      else{
-        hasil += daftarAngka[parseInt(strNumber[0]) - 1];
-        if(strNumber[0] !== '1')
-          hasil+= " ";
-        hasil += daftarKata[0] + " ";
-        strNumber.shift();
-      }
-      break;
-    case 3:
-      hasil += daftarAngka[parseInt(strNumber[0]) - 1];
-      if(strNumber[0] !== '1')
-        hasil+= " ";
-      hasil += daftarKata[1] + " ";
-      strNumber.shift();
-      break;
+  if(number<=999999){
+    return numberToWords(Math.floor(number/1000)) + " ribu " + numberToWords(number % 1000);
   }
-  var newNumber = parseInt(strNumber.join(''));
-  return hasil + numberToWords(newNumber);
+  if(number<=999999999){
+    return numberToWords(Math.floor(number/1000000)) + " juta " + numberToWords(number % 1000000);
+  }
+  if(number<=999999999999){
+    return numberToWords(Math.floor(number/1000000000)) + " milyar " + numberToWords(number % 1000000000);
+  }
+  if(number<=999999999999999){
+    return numberToWords(Math.floor(number/1000000000000)) + " triliun " + numberToWords(number % 1000000000000);
+  }
 }
 
 // Driver code
-console.log(numberToWords(999999999999999));
+console.log(numberToWords(1));
+console.log(numberToWords(10));
+console.log(numberToWords(11));
+console.log(numberToWords(20));
+console.log(numberToWords(100));
+console.log(numberToWords(211));
+console.log(numberToWords(1000));
+console.log(numberToWords(11000));
+console.log(numberToWords(111000));
+console.log(numberToWords(1111000));
+console.log(numberToWords(1001));
+console.log(numberToWords(5001));
+console.log(numberToWords(51000));
+console.log(numberToWords(1000000));
+console.log(numberToWords(1000000000000));
+console.log(numberToWords(1234567897524));
+console.log(numberToWords(1111111111));
+console.log(numberToWords(1111111111111));
 
- module.exports = {
-   numberToWords: numberToWords
- }
+module.exports = {
+  numberToWords: numberToWords
+}
