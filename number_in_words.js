6f36752b890da409e5a0d42b0ee65684f321daba
albertagung <albiardtya@gmail.com>
2017-09-18 23:38:17 +0700

    Perubahan kedua Release 0

--- number_in_words.js
+++ number_in_words.js
@@ -10,18 +10,22 @@ function numberToWords(number) {
     [8,"delapan"],
     [9,"sembilan"]
   ]
+  var pisah = number.toString().split("");
   console.log(arr[0][0])
   for(var i = 0; i < arr.length; i++){
     console.log(arr[i][0]);
     if(number === arr[i][0]){
-      
+      return arr[i][1];
+    }
+    if(number > 10 && number < 20 && parseInt(pisah[0]) === arr[i][0]){
+      return arr[i][1] + " belas"
     }
   }
 }
 
 // Driver code
 // console.log(numberToWords(1000000));
-console.log(numberToWords())
+console.log(numberToWords(12))
 
 module.exports = {
   numberToWords: numberToWords
