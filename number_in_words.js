fb8906f3dd7274f6193a204bb76106a0f278c3ba
name": null <email": null>
2017-09-18 21:04:32 +0700

    release 1-2 : number in word

--- number_in_words.js
+++ number_in_words.js
@@ -1,49 +1,72 @@
 function numberToWords(number) {
   // Your code here
-  var a = ['', 'satu', 'dua', 'tiga', 'empat', 'lima', 'enam', 'tujuh', 'delapan', 'sembilan'];
-  var b = ['ribu', 'juta', 'milyar', 'triliun'];
-  var numberStr = number.toString().split('');
-  var tampung = '';
-
-  if(number < 10){
-    // satuan
-    tampung += a[number];
+  var a = {
+    '0' : '',
+    '1' : 'satu',
+    '2' : 'dua',
+    '3' : 'tiga',
+    '4' : 'empat',
+    '5' : 'lima',
+    '6' : 'enam',
+    '7' : 'tujuh',
+    '8' : 'delapan',
+    '9' : 'sembilan',
+    '10' : 'sepuluh',
+    '11' : 'sebelas'
   }
+  var tampung = 0;
 
-  if(number === 10){
-    // sepuluh & sebelah
-    tampung += 'sepuluh';
-  }
-
-  if(number === 11){
-    tampung += 'sebelas';
+  if(number >= 0 && number <=11){
+    return a[number.toString()]
   }
 
   if(number > 11 && number < 20){
-    // belasan
-    tampung += a[numberStr[1]] + ' belas';
+    tampung = number%10;
+    return a[tampung.toString()] + ' belas';
   }
 
   if(number >= 20 && number < 100){
-    // puluhan
-    tampung += a[numberStr[0]] + ' puluh ' + a[numberStr[1]];
+    var angka = Math.floor(number/10);
+    return a[angka] + ' puluh ' + numberToWords(number%10);
   }
 
-  if(number >= 100 && number < 1000){
-    // ratusan
-    tampung += a[numberStr[0]] + ' ratus ' + a[numberStr[1]] + ' puluh ' + a[numberStr[2]];
+  if(number >= 100 && number < 200){
+    var angka = Math.floor(number/100);
+    return 'seratus ' + numberToWords(number%100);
   }
 
-  if(number >= 1000 && number < 1000000){
-    // ribuan
-    tampung += a[numberStr[0]] + ' ratus ' + a[numberStr[1]] + ' puluh ' + a[numberStr[2]];
+  if(number >= 200 && number < 1000){
+    var angka = Math.floor(number/100);
+    return a[angka] + ' ratus ' + numberToWords(number%100);
   }
 
-  return tampung;
+  if(number >= 1000 && number < 2000){
+    var angka = Math.floor(number/1000);
+    return 'seribu ' + numberToWords(number%1000);
+  }
+  if(number >= 2000 && number < 1000000){
+    var angka = Math.floor(number/1000);
+    return numberToWords(angka) + ' ribu ' + numberToWords(number%1000);
+  }
+
+  if(number >= 1000000 && number < 1000000000){
+    var angka = Math.floor(number/1000000);
+    return numberToWords(angka) + ' juta ' + numberToWords(number%1000000);
+  }
+
+  if(number >= 1000000000 && number < 1000000000000){
+    var angka = Math.floor(number/1000000000);
+    return numberToWords(angka) + ' miliar ' + numberToWords(number%1000000000);
+  }
+
+  if(number >= 1000000000000 && number < 1000000000000000){
+    var angka = Math.floor(number/1000000000000);
+    return numberToWords(angka) + ' triliun ' + numberToWords(number%1000000000000);
+  }
 }
 
 // Driver code
-console.log(numberToWords(999));
+console.log(numberToWords(111111111111111));
 
 module.exports = {
   numberToWords: numberToWords
