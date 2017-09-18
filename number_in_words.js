1a99f074cd271f4775847d049335bfcbb90f53c4
albertagung <albiardtya@gmail.com>
2017-09-18 23:26:55 +0700

    Percobaan Release 0 pertama yang sepertinya gagal

--- number_in_words.js
+++ number_in_words.js
@@ -1,9 +1,27 @@
 function numberToWords(number) {
-  // Your code here
+  var arr = [
+    [1,"satu"],
+    [2,"dua"],
+    [3,"tiga"],
+    [4,"empat"],
+    [5,"lima"],
+    [6,"enam"],
+    [7,"tujuh"],
+    [8,"delapan"],
+    [9,"sembilan"]
+  ]
+  console.log(arr[0][0])
+  for(var i = 0; i < arr.length; i++){
+    console.log(arr[i][0]);
+    if(number === arr[i][0]){
+      
+    }
+  }
 }
 
 // Driver code
-console.log(numberToWords(1000000));
+// console.log(numberToWords(1000000));
+console.log(numberToWords())
 
 module.exports = {
   numberToWords: numberToWords
