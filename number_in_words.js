e815182fe0f8c4dafe56c6d39b0600c9134bf319
Rahmat Hidayat <email": null>
2017-09-18 20:04:13 +0700

    number_in_words release 1

--- number_in_words.js
+++ number_in_words.js
@@ -1,9 +1,28 @@
 function numberToWords(number) {
   // Your code here
+  var angka = ['satu','dua','tiga','empat','lima','enam','tujuh','delapan','sembilan','sepuluh'];
+  var belas = ['sebelas', 'dua belas', 'tiga belas', 'empat belas', 'lima belas', 'enam belas', 'tujuh belas', 'delapan belas', 'sembilan belas'];
+  var satuan = ['belas','puluh','ratus','ribu'];
+
+  if(number <= 10){
+    return angka[number - 1];
+  }
+  else if(number > 10 && number < 19 ){
+    return belas[(number % 10)-1];
+  }
+  else if (number > 19 && number <= 99){
+    return angka[Math.floor((number/10)-1)] + ' puluh ' + numberToWords(number % 10);
+  }
+  else if (number > 99 && number < 1000){
+    return angka[Math.floor((number / 100)-1)] + ' ratus ' + numberToWords(number % 100);
+  }
+
 }
 
+
+
 // Driver code
-console.log(numberToWords(1000000));
+console.log(numberToWords(199));
 
 module.exports = {
   numberToWords: numberToWords
